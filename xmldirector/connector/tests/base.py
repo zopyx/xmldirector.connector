9a6c52559f08afd6f5135610d809dd6936b6a64e
Andreas Jung <info@zopyx.com>
2019-02-24 11:04:39 +0100

    fixed for Plone 5.2

--- xmldirector/connector/tests/base.py
+++ xmldirector/connector/tests/base.py
@@ -8,6 +8,8 @@
 import os
 import uuid
 import unittest
+import six
+
 import plone.api
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
@@ -28,7 +30,7 @@ from xmldirector.connector.logger import LOG
 import xmldirector.connector
 import plone.app.dexterity
 
-CONNECTOR_URL = os.environ.get('CONNECTOR_URL', 'http://localhost:6080/exist/webdav/db')
+CONNECTOR_URL = os.environ.get('CONNECTOR_URL', 'file:///tmp')
 CONNECTOR_USERNAME = os.environ.get('CONNECTOR_USERNAME', 'admin')
 CONNECTOR_PASSWORD = os.environ.get('CONNECTOR_PASSWORD', 'admin')
 
@@ -40,53 +42,52 @@ class PolicyFixture(PloneSandboxLayer):
     defaultBases = (PLONE_FIXTURE, )
 
     def setUpZope(self, app, configurationContext):
-        #        xmlconfig.file('meta.zcml', z3c.jbot, context=configurationContext)
-
         for mod in [
                 plone.app.dexterity,
                 xmldirector.connector,
         ]:
             xmlconfig.file('configure.zcml', mod, context=configurationContext)
 
-        # Install product and call its initialize() function
-
-
-#        z2.installProduct(app, 'eteaching.policy')
-
     def setUpPloneSite(self, portal):
         # Install into Plone site using portal_setup
-        applyProfile(portal, 'xmldirector.connector:testing')
+        applyProfile(portal, 'xmldirector.connector:default')
         portal.acl_users.userFolderAddUser('god', 'dummy', ['Manager'], [])
         portal.acl_users.userFolderAddUser('god2', 'dummy', ['Manager'], [])
         setRoles(portal, 'god', ['Manager'])
         setRoles(portal, 'god2', ['Manager'])
         login(portal, 'god')
 
+        self.testing_directory = 'testing-{}'.format(uuid.uuid4())
+
+        self.connector = plone.api.content.create(type='xmldirector.connector', container=plone.api.portal.get(), id='connector')
+
         registry = getUtility(IRegistry)
         settings = registry.forInterface(IConnectorSettings)
-        settings.connector_username = unicode(CONNECTOR_USERNAME)
-        settings.connector_password = unicode(CONNECTOR_PASSWORD)
-        settings.connector_url = unicode(CONNECTOR_URL)
-        self.testing_directory = settings.connector_dexterity_subpath = u'testing-dexterity-{}'.format(uuid.uuid4())
+        settings.connector_username = CONNECTOR_USERNAME
+        settings.connector_password = CONNECTOR_PASSWORD
+        settings.connector_url = CONNECTOR_URL 
 
-        handle = get_fs_wrapper(
-            CONNECTOR_URL, credentials=dict(username=CONNECTOR_USERNAME, password=CONNECTOR_PASSWORD))
+        handle = self.connector.get_handle()
+        handle.makedir(self.testing_directory)
+
+        settings.connector_url = CONNECTOR_URL + '/' + self.testing_directory
+
         if not handle.exists(self.testing_directory):
             handle.makedir(self.testing_directory)
 
-        self.connector = plone.api.content.create(
-            container=portal, type='xmldirector.plonecore.connector', id='connector')
+    def tearDownPloneSite(self, app):
+      
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IConnectorSettings)
+        settings.connector_url = CONNECTOR_URL 
 
-    def tearDownZope(self, app):
+        handle = self.connector.get_handle()
 
-        handle = get_fs_wrapper(
-            CONNECTOR_URL, credentials=dict(username=CONNECTOR_USERNAME, password=CONNECTOR_PASSWORD))
-        if handle.exists(self.testing_directory):
-            try:
-                handle.removedir(self.testing_directory, recursive=True, force=True)
-            except Exception as e:
-                LOG.error('tearDownZope() failed ({})'.format(e))
-        z2.uninstallProduct(app, 'xmldirector.plonecore')
+        try:
+            handle.removedir(self.testing_directory)
+        except Exception as e:
+            LOG.error('tearDownZope() failed ({})'.format(e))
+        z2.uninstallProduct(app, 'xmldirector.connector')
 
 POLICY_FIXTURE = PolicyFixture()
 POLICY_INTEGRATION_TESTING = IntegrationTesting(bases=(POLICY_FIXTURE, ), name='PolicyFixture:Integration')
