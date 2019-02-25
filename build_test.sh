083e2756ae759b3f6462c76478edbe7366ca6123
Andreas JungÂ <info@zopyx.com>
2019-02-25 09:35:04 +0100

    updated

--- build_test.sh
+++ build_test.sh
@@ -16,11 +16,11 @@ config=buildout-plone-$PLONE_VERSION.cfg
 docker run -d $DOCKER_OPTIONS $DOCKER
 
 #virtualenv --clear .
-python3 -m venv .
-bin/pip install setuptools==36.6.0
-bin/pip install zc.buildout
-bin/buildout bootstrap
-bin/buildout -c $config
+#python3 -m venv .
+pip install setuptools==36.6.0
+pip install zc.buildout
+buildout bootstrap
+buildout -c $config
 
 if [[ $TYPE  == 'OWNCLOUD' ]]
 then
